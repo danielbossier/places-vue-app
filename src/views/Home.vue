af09267c45e6bf1e524dafaf4135ba195b9050a1
danielbossier <dbossier21@gmail.com>
2021-07-20 23:27:50 -0700

    added show, create, update methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,36 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>New Places!</h1>
+    <div>
+      <p>
+        Name:
+        <input type="text" v-model="newPlaceParams.name" />
+      </p>
+      <p>
+        Address:
+        <input type="text" v-model="newPlaceParams.address" />
+      </p>
+    </div>
+    <button v-on:click="createPlace()">Create</button>
     <div v-for="place in places" :key="place.id">
       <h2>Name: {{ place.name }}</h2>
       <p>Address: {{ place.address }}</p>
+      <button v-on:click="showPlace(place)">More Info!</button>
     </div>
+    <dialog id="place-details">
+      <form method="dialog">
+        <h1>Place Info!</h1>
+        <p>
+          Name:
+          <input type="text" v-model="currentPlace.name" />
+        </p>
+        <p>
+          Address:
+          <input type="text" v-model="currentPlace.address" />
+        </p>
+        <button v-on:click="updatePlace(currentPlace)">Update</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
@@ -18,6 +44,8 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       places: {},
+      newPlaceParams: {},
+      currentPlace: {},
     };
   },
   created: function () {
@@ -28,6 +56,27 @@ export default {
       axios.get("http://localhost:3000/places").then((response) => {
         this.places = response.data;
         console.log("All places:", this.places);
+      });
+    },
+    createPlace: function () {
+      console.log("Adding a Place!");
+      axios
+        .post("http://localhost:3000/places", this.newPlaceParams)
+        .then((response) => {
+          console.log("Yay!", response.data);
+          this.places.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    showPlace: function (place) {
+      console.log(place);
+      this.currentPlace = place;
+      document.querySelector("#place-details").showModal();
+    },
+    updatePlace: function (place) {
+      var editPlaceParams = place;
+      axios.patch("http://localhost:3000/places/" + place.id, editPlaceParams).then((response) => {
+        console.log("Yippee!", response.data);
       });
     },
   },
