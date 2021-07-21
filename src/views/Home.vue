6ae19f41a47818aa2c057d4d05dbfb69b7929760
danielbossier <dbossier21@gmail.com>
2021-07-20 22:57:32 -0700

    added index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="place in places" :key="place.id">
+      <h2>Name: {{ place.name }}</h2>
+      <p>Address: {{ place.address }}</p>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
-  export default {
-    data: function () {
+import axios from "axios";
+
+export default {
+  data: function () {
     return {
-        message: "Welcome to Vue.js!",
-      };
+      message: "Welcome to Vue.js!",
+      places: {},
+    };
+  },
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("http://localhost:3000/places").then((response) => {
+        this.places = response.data;
+        console.log("All places:", this.places);
+      });
     },
-    created: function () {},
-    methods: {},
-  };
+  },
+};
 </script>
